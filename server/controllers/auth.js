5b42b7b68799676a05f4cd3f2b373a9b978eb3a4
Gavin Wilson <Gavin Wilson>
2017-02-20 14:13:42 -0500

    branch 5

---
+++ server/controllers/auth.js
@@ -0,0 +1,38 @@
+var User = require('../models/user');
+
+exports.signup = function(req, res, next){
+	//test 
+	res.send('authorization is happenin, yo');
+		//console.log(req.body);
+		//1
+		var email = req.body.email;
+		var password = req.body.password;
+		//2
+		User.findOne({ email: email}, function(err, existingUser){
+			if (err) {
+				return next(err);
+
+			}//handle error search
+
+			if(existingUser){
+				return res.status(418).send(err);
+				//alternative: return res.status(418).send('email is in use');
+			}//handles existing users
+
+		
+			//3
+			var user = new User({
+				email: email,
+				password: password
+			});
+
+			//to save the record to the db
+			user.save(function(err){
+				if(err) { return next(err); }
+			//4 Respod to request indicating the user was created
+			res.json({success: true});
+			});
+		});
+
+
+};
--- server/router.js
+++ server/router.js
@@ -1,9 +1,6 @@
-module.exports = function(app){
-	app.get('/', function(req, res, next){
-		res.send('hello homepage');
-	});
+var Auth = require('./controllers/auth');
+var User = require('./models/user');
 
-	app.get('/signup', function(req, res, next){
-		res.send('thanks for signing up');
-	});
+module.exports = function(app){
+	app.post('/signup', Auth.signup);
 };
