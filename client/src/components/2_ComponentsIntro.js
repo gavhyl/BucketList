0d14c31636fd1d7d3c6a73a464335ffce2a56c97
Gavin Wilson <Gavin Wilson>
2017-02-21 11:52:24 -0500

    commit 2

---
+++ client/src/components/2_ComponentsIntro.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class ComponentsIntro extends React.Component {
+	render() {
+		return (
+				<div>
+					<h3>Component Intro</h3>
+					<ul>
+						<li>Let you split the UI into independent, reusable pieces</li>
+						<li>Allow isolation of each piece</li>
+						<li>There is a component life cycle</li>
+						<li>All custom components should start with captal letters</li>
+						<li>We are creating instances of components in index.js</li>
+						
+					</ul>
+					<hr/>
+				</div>
+
+
+			);
+	}
+}
+
+export default ComponentsIntro;
