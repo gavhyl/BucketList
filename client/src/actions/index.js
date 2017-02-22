77256c42f6de91a5ce4f762a47bdc16a374e776a
Gavin Wilson <Gavin Wilson>
2017-02-22 14:59:17 -0500

    redux thunk

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export const CREATE_POSTS = 'CREATE_POSTS';
 
-const ROOT_URL = 'http://rest.learncode.academy/api/paul';
+//const ROOT_URL = 'http://rest.learncode.academy/api/paul';
+const ROOT_URL = 'http://localhost:3000';
 
 export function createPost(props) {
 	const request = axios.post(`${ROOT_URL}/posts`, props);
@@ -10,4 +11,10 @@ export function createPost(props) {
 		type: CREATE_POSTS,
 		payload: request
 	};
+}
+
+export function signinUser({ email, password}){
+	return function(dispatch){
+		axios.post(`${ROOT_URL}/signin`, {email, password})
+	}
 }
