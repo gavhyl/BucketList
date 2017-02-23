32b598d2826dfc62e22ea4cf4f012184f27bb882
Gavin Wilson <Gavin Wilson>
2017-02-23 10:58:30 -0500

    signout

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import App from './components/app';
 import reducers from './reducers';
 import { Router, Route, IndexRoute, browserHistory} from 'react-router';
 import Signin from './components/auth/signin';
+import Signout from './components/auth/signout';
 import ListItem from './components/list/new-list-item';
 
 var createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
@@ -17,6 +18,7 @@ ReactDOM.render(
 		<Router history={browserHistory}>
 			<Route path="/" component={App}>
 				<Route path="signin" component={Signin} />
+				<Route path="signout" component={Signout} />
 				<Route path="newitem" component={ListItem} />
 			</Route>
 		</Router>		
